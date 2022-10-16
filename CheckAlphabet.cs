8b824b38d622ad3438a66e7fa7ea0cdeae7577c7
pratishabhople <pratikshabhople26@gmail.com>
2022-10-16 19:39:39 +0530

    CheckAlphabet file

---
+++ CheckAlphabet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelAndConsonant
+{
+    public class CheckAlphabet
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Enter any character");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' )
+            {
+                Console.WriteLine(ch + "vowel");
+
+            } else if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + "is consonant.");
+            }
+            else
+            {
+                Console.WriteLine("Enter the correct character");
+            }
+
+
+            
+
+            
+        }
+    }
+}
